a3ebe409e0b139c708e727e0fc4a272b61f617d5
merlinkv <35397340+merlinkv@users.noreply.github.com>
2025-02-09 22:47:31 +0200

    Delete userconfig_blank.h

--- userconfig_blank.h
+++
@@ -1,5 +0,0 @@
-////////////////                CONFIG FOR ANY DEVICE                  //////////////////////////////
-// ONLY USE THIS IF YOU ARE SPECIFYING EVERY REQUIRED CONFIGURATION OPTION
-//  - via commandline
-//  - or via -D build_flags in platformio.ini
-
